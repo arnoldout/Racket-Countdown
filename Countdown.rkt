b0b33269b3189d9bb8905c256c7b5e4a617c1694
arnoldout <arnoldout@gmail.com>
2017-03-04 12:29:16 +0000

    added a generic function and implemented add and multiply

--- Countdown.rkt
+++ Countdown.rkt
@@ -1,3 +1,13 @@
 #lang racket
 (define l(list 1 2))
-(permutations (list 1 2 3))
+(define perms(list(permutations (list 1 2))))
+perms
+
+(define (f oper id l)
+  (if (null? l)
+      id
+      (oper (car l) (f oper id (cdr l)))))
+
+(define (fsum l) (f + 0 l))
+(define (fmult l) (f * 0 l))
+(fmult (car (car perms)))
