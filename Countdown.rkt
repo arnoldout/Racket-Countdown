d223a64161a46f94c267eeb856acefdc5f6158f3
arnoldout <arnoldout@gmail.com>
2017-04-14 22:13:13 +0100

    Merged into countdown file

--- Countdown.rkt
+++ Countdown.rkt
@@ -1,6 +1,8 @@
 #lang racket
 (require data/queue)
 
+(define generatedVals(list 3 5 7 2 4 1))
+(define operands (list + - * /))
 (define (make-rpn l)
   (append (list 1 1) l (list -1)))
 
@@ -17,6 +19,8 @@
                [(equal? (car expression)-1)(isValidRPN (cdr expression) (- stack 1))]
                [else (isValidRPN (cdr expression) (stack))]
                ))))
+
+
 (define (convertToValues binaryList values operands [mappedVals '()] )
   (if(null? binaryList)
      mappedVals
@@ -25,10 +29,14 @@
         (convertToValues  (cdr binaryList) values operands (cartesianOnList operands mappedVals))
         )
   ))
+
+
 (define  (cartesianOnList list value)
   (define a (cartesian-product list value))
   (define b(list 2 3))
   a)
+
+
 (define (evaluateRPN expression [stack (make-queue)])
      (if (null? expression)
          (if (=(queue-length stack) 1)
@@ -38,12 +46,36 @@
          #f)
          (if (number? (car expression))
          (evaluateRPN (cdr expression) (enqueueAndReturn stack (car expression)))
-         ;do rpn calculating, which also returns a boolean
+         ;do rpn calculating, which also returns a boolean 
          ;a true means that the calculation was valid
          ;a false means the calculation returned either a negative number or fraction, which is invalid
          (if (doRPN stack (car expression))
              (evaluateRPN (cdr expression) stack)
              #f))))
+
+
+(define (cart li evalType [vals '()])
+  (if (null? li)
+     vals
+     (if(equal? evalType 1)
+         (if (pair?(car li))
+             (cart(cdr li) evalType (append (cartesian-product (list(flatten (car li)))(remove* (flatten (car li))generatedVals))vals))
+             (cart(cdr li) evalType (append (cartesian-product (list (car li))(remove (car li) generatedVals))vals)))
+         
+         (cart(cdr li) evalType (append (cartesian-product (list(flatten (car li)))operands)vals))
+         )
+         ))
+
+
+
+(define (cartManager evalList [vals '()])
+  (define li generatedVals)
+  (if (null? evalList)
+      vals
+      (if (empty? vals)
+      (cartManager (cdr evalList) (append(cart li (car evalList))vals))    
+      (cartManager (cdr evalList) (append(cart vals (car evalList))vals)))))
+
 
 (define (enqueueAndReturn stack value)
   (enqueue-front! stack value)
@@ -67,13 +99,9 @@
 (make-rpn (car  perms))
 
 (define lll(map make-rpn perms))
+(define llll (map cartManager lll))
+
 (define total (random 100 1000))
-total
-;(define qqq(filter isValidRPN lll));
-;qqq
-(define ff (list 3 5 + 7 2 - *))
 
-(define qqqq(evaluateRPN ff))
-qqqq
-
-
+;(define qqqq(map evaluateRPN llll))
+;qqqq
