59bb90bf286b39448b47ec98249fbfd07a2e5d6b
arnoldout <arnoldout@gmail.com>
2017-04-14 11:32:41 +0100

    removed dud code

--- Countdown.rkt
+++ Countdown.rkt
@@ -2,15 +2,6 @@
 (require data/queue)
 (define l(list 1 1 -1 1 1 1 1 -1 -1 -1 -1))
 ;(define perms(list(permutations (list 1 1 1 1 1 -1 -1))))
-
-(define (f oper id l)
-  (if (null? l)
-      id
-      (oper (car l) (f oper id (cdr l)))))
-
-(define (fsum l) (f + 0 l))
-(define (fmult l) (f * 0 l))
-;(fmult (car (car perms)))
 (define q (make-queue))
 
 (define start-perm (list -1 -1 -1 -1 1 1 1 1))
@@ -23,8 +14,8 @@
 (make-rpn (car  perms))
 
 (define lll(map make-rpn perms))
-(define total 40)
-
+(define total (random 100 1000))
+total
 (define (isValidRPN expression [stack 0])
   (if(< stack 0)
      #f
